d3c8d4dadece4fbea5e1d5d6ec22f73f1d53564d
fef <fitrah.firdaus@gmail.com>
2023-11-02 14:21:20 +0700

    Create Login Form

---
+++ src/main/java/com/smk/bi/views/LoginView.java
@@ -0,0 +1,26 @@
+package com.smk.bi.views;
+
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.login.LoginForm;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.auth.AnonymousAllowed;
+
+@Route("login")
+@PageTitle("LOGIN")
+@AnonymousAllowed
+public class LoginView extends VerticalLayout {
+    
+    private final LoginForm login = new LoginForm();
+    public LoginView() {
+        addClassName("login-view");
+        setSizeFull();
+        setAlignItems(Alignment.CENTER);
+        setJustifyContentMode(JustifyContentMode.CENTER);
+
+        login.setAction("login");
+
+        add(new H1("Login Page"), login);
+    }
+}
